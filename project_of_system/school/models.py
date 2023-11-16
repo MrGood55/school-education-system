662d9ed3f124fca93329c6e053e41bb21d341e10
MrGood55 <72146326+MrGood55@users.noreply.github.com>
2023-11-16 12:48:01 +0300

    1

--- project_of_system/school/models.py
+++ project_of_system/school/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+
+    name = models.CharField()
+    second_name = models.CharField()
+    third_name = models.CharField()
+    email = models.EmailField()
+
+    group = models.ForeignKey()
+    school = models.ForeignKey()
+    
+class Personal(models.Model):
+    name = models.CharField()
+    second_name = models.CharField()
+    third_name = models.CharField()
+    email = models.EmailField()
+    position = models.CharField()
+
+    school = models.ForeignKey()
+
+
+class Subject(models.Model):
+    title = models.CharField()
+
+
