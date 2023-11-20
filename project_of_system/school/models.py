8b3ab0d47bb7140e43eaac847c2fbb351abc5895
Bandos <72146326+MrGood55@users.noreply.github.com>
2023-11-20 12:07:42 +0300

    Update models.py

--- project_of_system/school/models.py
+++ project_of_system/school/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 
+from django.db import models
+
 class School(models.Model):
     name = models.CharField(max_length=100)
     # Other school-related fields as needed
@@ -10,66 +12,48 @@ class School(models.Model):
     def __str__(self):
         return self.name
 
-class Staff(models.Model):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    patronymic = models.CharField(max_length=50, blank=True, null=True)
-    email = models.EmailField()
-    role = models.CharField(max_length=50)  # e.g., Principal, Administrator, etc.
-    school = models.ForeignKey(School, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.name} {self.surname} - {self.role}"
-
-class Student(models.Model):
+class Person(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     patronymic = models.CharField(max_length=50, blank=True, null=True)
     email = models.EmailField()
     school = models.ForeignKey(School, on_delete=models.CASCADE)
-    # Other student-related fields as needed
+    status = models.CharField(max_length=20, choices=[
+        ('student', 'Student'),
+        ('parent', 'Parent'),
+        ('teacher', 'Teacher'),
+        ('special_staff', 'Special Staff'),
+        ('administrator', 'Administrator'),
+    ])
 
     def __str__(self):
-        return f"{self.name} {self.surname}"
-
-class Parent(models.Model):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    patronymic = models.CharField(max_length=50, blank=True, null=True)
-    email = models.EmailField()
-    # Other parent-related fields as needed
-
-    def __str__(self):
-        return f"{self.name} {self.surname}"
+        return f"{self.name} {self.surname} - {self.status}"
 
 class Subject(models.Model):
     name = models.CharField(max_length=100)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
-    # Other subject-related fields as needed
 
     def __str__(self):
         return self.name
 
 class Class(models.Model):
     class_name = models.CharField(max_length=50)
-    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
-    students = models.ManyToManyField(Student)
+    staff = models.ForeignKey(Person, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Person, related_name='enrolled_classes', limit_choices_to={'status': 'student'})
     subjects = models.ManyToManyField(Subject)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
-    # Other class-related fields as needed
 
     def __str__(self):
         return self.class_name
 
 class Evaluation(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey(Person, on_delete=models.CASCADE, limit_choices_to={'status': 'student'})
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    staff = models.ForeignKey(Person, on_delete=models.CASCADE, limit_choices_to={'status__in': ['teacher', 'special_staff', 'administrator']})
     grade = models.DecimalField(max_digits=5, decimal_places=2)
     date = models.DateField()
-    # Other evaluation-related fields as needed
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.student} - {self.subject} - {self.staff} - {self.grade} ({self.date})"
-
-
