0eb8552acce6f7f9f388115ffedc6575f202e380
Bandos <72146326+MrGood55@users.noreply.github.com>
2023-11-21 16:38:06 +0300

    Update models.py

--- project_of_system/school/models.py
+++ project_of_system/school/models.py
@@ -8,7 +8,12 @@ from django.db import models
 
 class School(models.Model):
     name = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    region = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=255)
     # Other school-related fields as needed
+
 
     def __str__(self):
         return self.name
